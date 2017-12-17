c99c16cc678dfe1a460a159e75989c978de75e88
Markham <quinesquines@gmail.com>
2017-12-17 14:17:52 -0800

    ignore whitespace on diff; log only on success

--- sendall.sh
+++ sendall.sh
@@ -2,16 +2,17 @@
 
 LOG=sent.tsv
 touch "$LOG"
-diff "$LOG" ../email-addresses.tsv \
+diff -w "$LOG" ../email-addresses.tsv \
 | grep -P '^>' \
 | perl -pe 's/^> //' \
 | while read -r line; do
-	name=$(cut -f1 <<< "$line")
-	home=$(cut -f2 <<< "$line")
+	name="$(cut -f1 <<< "$line")"
+	home="$(cut -f2 <<< "$line")"
 	IFS=$'\t' read -r -a addrs < <(cut -f3- <<< "$line")
 	printf "\n$name\t$home"  >> "$LOG"
 	for addr in "${addrs[@]}"; do
-		echo addr $addr
-		printf "\t$addr" >> "$LOG"
-	done 
+		if ./sendone.sh aggierc "$name" "$addr"; then
+			printf "\t$addr" >> "$LOG"
+		fi
+	done
 done
