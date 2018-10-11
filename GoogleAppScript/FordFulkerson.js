df4b55c96f654316ed42b8139bbe954d7ddd2695
Markham <quinesquines@gmail.com>
2018-10-11 13:20:33 -0700

    for PriorityQueue, take comparator function as argument

--- GoogleAppScript/FordFulkerson.js
+++ GoogleAppScript/FordFulkerson.js
@@ -9,14 +9,6 @@ function Edge (src, dst, capacity, flow) {
 	this.flow = flow;
 }
 
-function edgePair (src, dst, capacity, flow) {
-	var a = new Edge(src, dst, capacity, flow);
-	var b = new Edge(dst, src, capacity, capacity-flow);
-	a.reciprocal = b;
-	b.reciprocal = a;
-	return [a,b];
-}
-
 function ResidualNetwork (sessions, users) {
 	this.users    = users.map(function (u) { return new Node(u) });
 	this.sessions = sessions.map(function (u) { return new Node(u) });
@@ -38,33 +30,47 @@ Object.defineProperties(ResidualNetwork.prototype, {
 
 	},
 	// todo : define node.edges as a priority queue
+	// todo define waitlist on user
+	// todo don't assign to most preferred yet
 	'initUserEdges': {
 		value: function () {
+			// Initialize priority queue for users (entering sink)
+			this.sink.inboundEdges = PriorityQueue(function (edgeA, edgeB) {
+				var a = edgeA.src; // user A
+				var b = edgeB.src; // user B
+				var d =
+					(a.registrations.length - b.registrations.length) // favour low ct
+					|| (a.timestamp - b.timestamp) // favour low timestamp
+					|| -(a.isMember - b.isMember); // favour members
+				return d <= 0;
+			});
+			// Create edges
 			for (var i in this.users) {
 				var user = this.users[i];
 				var edgeToSink = this.addEdgePair(user, this.sink, user.data.maxRegistrations, 0);
-				for (var h in lists) {
-					var flow = 1 - h; // registrations have flow; waitlists need to wait on pathfinding
-					var list = lists[h];
-					for (var j in list) {
-						var sess = list[j];
-						if (user.data.needsBow) {
-							if (user.data.needsBow == RIGHT) {
-								this.addEdgePair(this.R[sess], user, 1, flow);
-								this.addEdgePair(this.X[sess], this.R[sess], 1, flow)
-							} else if (user.data.needsBow == LEFT) {
-								this.addEdgePair(this.L[sess], user, 1, flow);
-								this.addEdgePair(this.X[sess], this.L[sess], 1, flow)
-							} else {
-								throw new Exception('badddd');
-							}
+				for (var j in user.waitlist) {
+					var sess = user.waitlist[j];
+
+					// Set inbound edges based on resources X, R, L
+					if (user.data.needsBow) {
+						if (user.data.needsBow == RIGHT) {
+							this.addEdgePair(this.R[sess], user, 1, 0);
+							this.addEdgePair(this.X[sess], this.R[sess], 1, 0)
+						} else if (user.data.needsBow == LEFT) {
+							this.addEdgePair(this.L[sess], user, 1, 0);
+							this.addEdgePair(this.X[sess], this.L[sess], 1, 0)
 						} else {
-							this.addEdgePair(this.X[sess], user, 1, flow);
+							throw new Exception('badddd');
 						}
-						this.getEdge(this.source, this.X[sess]).flow += flow;
-						this.getEdge(user, this.sink).flow += flow;
+					} else {
+						this.addEdgePair(this.X[sess], user, 1, 0);
 					}
+
 				}
+
+				// Set outbound edges (to sink) based on maxRegistrations
+				var user2Sink = this.addEdgePair(user, this.sink, user.maxRegistrations, 0);
+				this.sink.inboundEdges.push(user2Sink);
 			}
 		},
 		'getEdge': {
@@ -82,24 +88,28 @@ Object.defineProperties(ResidualNetwork.prototype, {
 			},
 		},
 		'pathfind': {
+			// Do search from sink to source so that priority queue has users all at the same level
 			value: function () {
-				var queue = [this.source];
-				var hist  = {this.source:true}; // node => inbound edge
+				var queue = [this.sink]; // search frontier
+				var hist  = {this.sink:true}; // node => outbound edge
 				while (queue.length()) {
 					var node = queue.shift();
 					if (hist[node]) continue;
-					for (e in node.edges) {
-						var edge = node.edges[e];
-						if (hist[edge.dst]) continue;
-						hist[edge.dst] = edge;
+					var inQueue = node.inboundEdges;
+					var outQueue = 
+					while ( ! pQueue.isEmpty() ) {
+						var edge = pQueue.pop();
+						if (hist[edge.src]) continue;
+						hist[edge.src] = edge;
 						if (edge.capacity == edge.flow) continue;
-						queue.push(edge.dst);
-						if (edge.dst == this.sink) {
+						queue.push(edge.src);
+						if (edge.src == this.source) {
 							this.path = [];
-							for (var u = edge.dst; u != this.source; u = hist[u].src)
+							for (var u = edge.src; u != this.sink; u = hist[u].dst)
 								this.path.push(hist[u]);
 							return true;
 						}
+						if (edge.isEligible()) pQueue.push(edge);
 					}
 				}
 				return false;
--- GoogleAppScript/Heap.js
+++ GoogleAppScript/Heap.js
@@ -1,85 +1,86 @@
-// Holds priority queue of users
-function Heap (arrayOfUsers) {
-    var n = arrayOfUsers.length;
+// Holds prioirity queue of anything
+function PriorityQueue (lessThanFunction, n) {
     this.arr = Array(n);
     this.last = -1;
-    for (var i in arrayOfUsers) {
-        this.push(arrayOfUsers[i]);
-    }
+    this.lt = lessThanFunction; // Return true if element at idxA gets priority over element at idxB
 }
 
 (function () {
+
     function parentIdx (i) {
         return parseInt((i-1) / 2);
     }
-    
+
     function leftChildIdx (i) {
         return i * 2 + 1;
     }
-    
-    function compare(a, b) {
-        return (
-            (a.registrations.length - b.registrations.length) // favour low ct
-            || (a.timestamp - b.timestamp) // favour low timestamp
-            || -(a.isMember - b.isMember) // favour members
-        );
-    }
 
-    Heap.prototype.isEmpty = function () {
-        return this.last < 0;
-    }
+    Object.defineProperties(PriorityQueue.prototype, {
+        'isEmpty': {
+            value: function () {
+                return this.last < 0;
+            },
+        },
+        'push': {
+            value: function (user) {
+                var i = ++this.last;
+                this.arr[i] = user;
+                this.trickleUp(i);
+            },
+        },
+        'pop': {
+            value: function () {
+                var ret = this.arr[0];
+                this.arr[0] = this.arr[this.last];
+                this.last --;
+                this.trickleDown(0);
+                return ret;
+            },
+        },
+        'swap': {
+            value: function (idxA, idxB) {
+                var tmp = this.arr[idxA];
+                this.arr[idxA] = this.arr[idxB];
+                this.arr[idxB] = tmp;
+            },
+        },
+        'trickleUp': {
+            value: function (i) {
+                var parI = parentIdx(i);
+                if (this.lt(i, parI)) {
+                    this.swap(i, parI);
+                    this.trickleUp(parI);
+                }
+            },
+        },
+        'trickleDown': {
+            value: function (i) {
+                var childI;
+                var lI = leftChildIdx(i);
+                if (lI > this.last) return;
+                var rI = lI + 1;
+                if (rI > this.last)
+                    childI = lI;
+                else
+                    childI = this.arr[lI] < this.arr[rI] ? lI : rI;
+                if (this.lt(childI, i)) {
+                    this.swap(i, childI);
+                    this.trickleDown(childI);
+                }
+            },
+        },
+    });
 
-    Heap.prototype.push = function (user) {
-        var i = ++this.last;
-        this.arr[i] = user;
-        this.trickleUp(i);
-    }
-    
-    Heap.prototype.pop = function () {
-        var ret = this.arr[0];
-        this.arr[0] = this.arr[this.last];
-        this.last --;
-        this.trickleDown(0);
-    	return ret;
-    }
-    
-    // Return true if element at idxA gets priority over element at idxB
-    Heap.prototype.lt = function (idxA, idxB) {
-        return compare(this.arr[idxA], this.arr[idxB]) < 0;
-    }
-    
-    Heap.prototype.swap = function (idxA, idxB) {
-        var tmp = this.arr[idxA];
-        this.arr[idxA] = this.arr[idxB];
-        this.arr[idxB] = tmp;
-    }
-    
-    Heap.prototype.trickleUp = function (i) {
-        var parI = parentIdx(i);
-        if (this.lt(i, parI)) {
-            this.swap(i, parI);
-            this.trickleUp(parI);
-        }
-    }
-    
-    Heap.prototype.trickleDown = function (i) {
-        var childI;
-        var lI = leftChildIdx(i);
-        if (lI > this.last) return;
-        var rI = lI + 1;
-        if (rI > this.last)
-            childI = lI;
-        else
-            childI = this.arr[lI] < this.arr[rI] ? lI : rI;
-        if (this.lt(childI, i)) {
-            this.swap(i, childI);
-            this.trickleDown(childI);
-        }
-    }
 })();
 
-// var a = [1,4,8,3,8,0,5,3,2,7,99,2,5,31,7];
-// var h = new Heap(a);
-// while (!h.isEmpty()) {
-//     console.log(h.pop())
-// }
+
+var a = [1,4,8,3,8,0,5,3,2,7,99,2,5,31,7];
+var h = new PriorityQueue(function  (idxA, idxB) {
+           return this.arr[idxA] - this.arr[idxB] < 0;
+      
+}, a.length);
+for (var i in a)
+    h.push(a[i]);
+while (!h.isEmpty()) {
+    console.log(h.pop())
+}
